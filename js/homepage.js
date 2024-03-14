a4b01622d8caf871a96eda6906256dafa97de153
pescarualex <pescarualexflorin@gmail.com>
2024-03-14 19:07:09 +0200

    Added more structure in Homapege

--- js/homepage.js
+++ js/homepage.js
@@ -5,10 +5,10 @@ let API_URL = "http://localhost:8089/homepage";
 
   $(document).ready(function() {
     $.ajax({
-      url: API_URL + "/tasks-number",
+      url: API_URL + "/total-tasks-number",
       type: 'GET',
       success: function(data) {
-        $('.all-tasks-card #tasks-number').text("Total Tasks: " + data);
+        $('.all-tasks-card #total-tasks-number').text(data);
         //createTask();
       },
       error: function(xhr, textStatus, errorThrown) {
